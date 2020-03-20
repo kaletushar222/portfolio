2bc424651d06d39264231bc6d8d1d12f6fbf8ce3
Tushar Kale <tushar@procmart.com>
2020-03-21 00:31:36 +0530

    initial project commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardLayoutHome from './client/dashboard/layouts/home'
+
+function App() {
+  return (
+    <div className="App">
+      <DashboardLayoutHome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/client/dashboard/layouts/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ComponentHomeSideBar from '../components/home/SideBar'
+
+class DashboardLayoutHome extends Component {
+	render() {
+	    return (
+	    	<div>
+	    		<ComponentHomeSideBar/>
+	    	</div>
+	    )
+	}
+}
+
+export default DashboardLayoutHome;
